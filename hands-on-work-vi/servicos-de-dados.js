ba6a0518822040b306357b48ee8c86dd2c9c64bf
rafael <rafaelqg@univali.br>
2026-06-10 20:24:06 -0300

    aula 4

--- hands-on-work-vi/servicos-de-dados.js
+++ hands-on-work-vi/servicos-de-dados.js
@@ -1,5 +1,15 @@
 
 const mysql = require('mysql2');
+const express = require('express');
+const bodyParser = require('body-parser'); // npm install body-parser
+
+
+const app = express();
+app.listen(4333);
+
+const jsonParser = bodyParser.json() // create application/json parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false }) // create application/x-www-form-urlencoded parser
+
 
 //initialize mysql connection
 const MYSQL_IP="localhost";
@@ -19,7 +29,8 @@ const con = mysql.createConnection({
 
 
 
-function listaJogadores(){
+
+app.get('/get_jogadores', function (request, response) {
     con.connect(
         function(err) {
             if (err) throw err;
@@ -28,15 +39,20 @@ function listaJogadores(){
                 function (err, result) {
                     if (err) throw err;
                     const jogadoresJSON = JSON.stringify(result);
-                    console.log(jogadoresJSON);
+                    addCorsHttpHeaders(response);
+                    response.status(200);
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(jogadoresJSON);
                 });
         }
     );
-}
+
+});
+
 
 //listaJogadores();
+app.get('/get_jogos', function (request, response) {
 
-function listaJogos(){
     con.connect(
         function(err) {
             if (err) throw err;
@@ -45,14 +61,17 @@ function listaJogos(){
                 function (err, result) {
                     if (err) throw err;
                     const jogosJSON = JSON.stringify(result);
-                    console.log(jogosJSON);
+                    addCorsHttpHeaders(response);
+                    response.status(200);
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(jogosJSON);
                 });
         }
     );
-}
+});
 //listaJogos();
 
-function listaFormatos(){
+app.get('/get_formatos', function (request, response) {
      con.connect(
         function(err) {
             if (err) throw err;
@@ -60,15 +79,26 @@ function listaFormatos(){
             con.query(sql,[], 
                 function (err, result) {
                     if (err) throw err;
-                    const jogosJSON = JSON.stringify(result);
-                    console.log(jogosJSON);
+                    const formatosJSON = JSON.stringify(result);
+                    addCorsHttpHeaders(response);
+                    response.status(200);
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(formatosJSON);
                 });
         }
     );
 }
+);
 
 
-function inserePartida(dtInicio, dtFim, idJogadorA, idJogadorB, idVencedor, idFormato, idJogo){
+app.post('/insere_partida', urlencodedParser, function (request, response) {
+    inserePartida(con, request.body.data_inicio + " " + request.body.hora_inicio, request.body.data_fim+ " " + request.body.hora_fim, request.body.jogador_1, request.body.jogador_2, request.body.vencedor, request.body.formato, request.body.jogo);
+    response.status(200);
+    response.send("Partida registrada com sucesso!");
+}); 
+
+
+function inserePartida(con, dtInicio, dtFim, idJogadorA, idJogadorB, idVencedor, idFormato, idJogo){
      con.connect(
         function(err) {
             if (err) throw err;
@@ -83,4 +113,11 @@ function inserePartida(dtInicio, dtFim, idJogadorA, idJogadorB, idVencedor, idFo
 }
 
 //listaFormatos();
-inserePartida("2026-05-27 10:00:00", "2026-05-27 10:28:00", 1, 2, 1, 1, 1);
+//inserePartida("2026-05-27 10:00:00", "2026-05-27 10:28:00", 1, 2, 1, 1, 1);
+
+function addCorsHttpHeaders(httpResponse){
+  httpResponse.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  httpResponse.setHeader("Access-Control-Allow-Methods","POST,GET,OPTIONS,PUT,DELETE,HEAD");
+  httpResponse.setHeader("Access-Control-Allow-Headers","X-PINGOTHER,Origin,X-Requested-With,Content-Type,Accept");
+  httpResponse.setHeader("Access-Control-Max-Age","1728000");
+}
